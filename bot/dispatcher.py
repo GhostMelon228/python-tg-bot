b17fcf1e87d8f32dfc8b81dbae5e45627dbaebeb
Dima <karpovich_dima08@mail.ru>
2024-05-04 15:08:45 +0300

    create common apps

--- bot/dispatcher.py
+++ bot/dispatcher.py
@@ -11,4 +11,5 @@ def setup_dispatcher(dp):
 
 
 n_workers = 0 if DEBUG else 4
-dispatcher = setup_dispatcher(Dispatcher(bot, update_queue=None, workers=n_workers, use_context=True))
+dispatcher = setup_dispatcher(Dispatcher(
+    bot, update_queue=None, workers=n_workers, use_context=True))
